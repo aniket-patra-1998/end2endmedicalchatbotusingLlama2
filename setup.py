f485d28f5a900e04e0198c2621b6a3c31cec59bd
aniket-patra-1998 <aniketpatra001@gmail.com>
2024-04-04 01:41:05 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'end2endmedicalchatbotusingLlama2',
+    version = "0.0.1",
+    author = "Aniket Patra",
+    author_email = "aniketpatra001@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+
+)
